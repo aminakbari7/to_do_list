532fc6addc11c1e1a8b3e99d65efcde541095b7c
aminakbari7 <aminakbari733@gmail.com>
2024-03-05 13:41:28 +0330

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ from  tkinter import *
 import tkinter.messagebox
 #function to enter the task in the Listbox
 def entertask():
-    #A new window to pop up to take input
+    #A new root to pop up to take input
     input_text=""
     def add():
         input_text=entry_task.get(1.0, "end-1c")
@@ -11,7 +11,7 @@ def entertask():
             tkinter.messagebox.showwarning(title="Warning!",message="Please Enter some Text")
         else:
             listbox_task.insert(END,input_text)
-            #close the root1 window
+            #close the root1 root
             root1.destroy()
     root1=Tk()
     root1.title("Add task")
@@ -20,15 +20,12 @@ def entertask():
     button_temp=Button(root1,text="Add task",command=add)
     button_temp.pack()
     root1.mainloop()
-    
-
 #function to facilitate the delete task from the Listbox
 def deletetask():
     #selects the slected item and then deletes it 
     selected=listbox_task.curselection()
     listbox_task.delete(selected[0])
 #Executes this to mark completed 
-
 def markcompleted():
     marked=listbox_task.curselection()
     temp=marked[0]
@@ -39,38 +36,29 @@ def markcompleted():
     #delete it then insert it 
     listbox_task.delete(temp)
     listbox_task.insert(temp,temp_marked)
-#creating the initial window
-window=Tk()
+#creating the initial root
+root=Tk()
 #giving a title
-window.title("DataFlair To_Do_APP")
-
-
+root.title("DataFlair To_Do_APP")
 #Frame widget to hold the listbox and the scrollbar
-frame_task=Frame(window)
+frame_task=Frame(root)
 frame_task.pack()
-
 #to hold items in a listbox
 listbox_task=Listbox(frame_task,bg="black",fg="white",height=15,width=50,font = "Helvetica")  
 listbox_task.pack(side=tkinter.LEFT)
-
-#Scrolldown in case the total list exceeds the size of the given window 
+#Scrolldown in case the total list exceeds the size of the given root 
 scrollbar_task=Scrollbar(frame_task)
 scrollbar_task.pack(side=tkinter.RIGHT,fill=tkinter.Y)
 listbox_task.config(yscrollcommand=scrollbar_task.set)
 scrollbar_task.config(command=listbox_task.yview)
 
-
 #Button widget 
-entry_button=Button(window,text="Add task",width=50,command=entertask)
+entry_button=Button(root,text="Add task",width=50,command=entertask)
 entry_button.pack(pady=3)
-
-delete_button=Button(window,text="Delete selected task",width=50,command=deletetask)
+delete_button=Button(root,text="Delete selected task",width=50,command=deletetask)
 delete_button.pack(pady=3)
-
-mark_button=Button(window,text="Mark as completed ",width=50,command=markcompleted)
+mark_button=Button(root,text="Mark as completed ",width=50,command=markcompleted)
 mark_button.pack(pady=3)
-
-
-window.mainloop()
+root.mainloop()
 
 
